470d3dfb59fc425433beddba3a10ae12ac5cfd96
DESA KUMAARAN <desakumaaran3@gmail.com>
2017-02-16 12:16:59 +0530

    upload php hint

--- upload.php
+++ upload.php
@@ -3,6 +3,7 @@ if(is_array($_FILES)) {
 if(is_uploaded_file($_FILES['userImage']['tmp_name'])) {
 $sourcePath = $_FILES['userImage']['tmp_name'];
 $targetPath = "uploads/".$_FILES['userImage']['name'];
+    //target upolad folder
 if(move_uploaded_file($sourcePath,$targetPath)) {
 ?>
 <img src="<?php echo "";echo $targetPath; ?>" width="100px" height="100px" />
